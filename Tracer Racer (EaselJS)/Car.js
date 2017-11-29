3a1511b5c65fdb0f1d325d2967bc2bc29b32a6eb
Azer7 <Azer7@users.noreply.github.com>
2017-11-28 21:10:27 -0700

    new easeljs tracer

---
+++ Tracer Racer (EaselJS)/Car.js
@@ -0,0 +1,123 @@
+class Car {
+    constructor(x, y, rayAmount) {
+        this.speed = .3;
+        this.slowDown = 1;
+        this.pos = new Vector(x, y);
+        this.vel = new Vector(0, 0);
+        this.acc = new Vector(0, 0);
+        this._angle = 0;
+        this.crashed = false;
+
+        this.forwardWay = true;
+        this.startDate = new Date().getTime();
+        this.currentTime = 0;
+        this.bestTime = "N/A";
+        this.loops = 0;
+
+        this.rayNum = rayAmount;
+        this.rays = [];
+
+        for (let i = 0; i < rayAmount; i++)
+            this.rays.push(new Ray(0, 0, i * (360 / rayAmount)));
+    }
+
+    get angle() {
+        let inDegrees = this._angle * 180 / Math.PI;
+        return inDegrees; //5 decimals of precision
+    }
+    set angle(degrees) {
+        //change to radians
+        //360 - degrees to switch it from clockwise to counter clockwise
+        if (!this.crashed)
+            this._angle = degrees * Math.PI / 180;
+    }
+
+    process(objArr) {
+        if (!this.crashed) {
+            this.currentTime = (new Date().getTime() - car.startDate) / 1000;
+
+            //add movement
+            this.acc.rotate(this._angle);
+            this.vel.add(this.acc.div(this.slowDown));
+            if (this.acc.x == 0 && this.acc.y == 0) {
+                this.vel.mult(.88);
+            } else {
+                this.vel.mult(.985);
+            }            
+            if (this.pos.x > 50 && this.pos.x < 430 && this.pos.y >= 600 && this.pos.y + this.vel.y < 600) {
+                if (this.forwardWay) {
+                    this.startDate = new Date().getTime();
+                    if (this.loops == 0 || this.currentTime < this.bestTime)
+                        this.bestTime = this.currentTime;                   
+
+                    this.loops++;
+                }
+                else {
+                    this.forwardWay = true;
+                    this.startDate = new Date().getTime();
+                }
+            } else if (this.pos.x > 50 && this.pos.x < 430 && this.pos.y <= 600 && this.pos.y + this.vel.y > 600) {
+                this.forwardWay = false;
+            }
+            this.pos.add(this.vel);
+            this.acc.mult(0); // only do once
+            this.currentTime += 0.016;
+
+            //process rays
+//            for (let i = 0; i < this.rays.length; i++) {
+//                this.rays[i].pos = this.pos;
+//                this.rays[i]._angle = this._angle + this.rays[i].baseAngle;
+//            }
+            let totalVector = new Vector(0, 0);
+            
+            for (let i = 0; i < this.rays.length; i++) {
+             
+                this.rays[i].pos = this.pos;
+               this.rays[i].checkCollisions(objArr);
+                //calculate collision bounce back direction
+                let largestVector = new Vector(-1, this.rays[i].slope);
+                if (this.rays[i].angle > 270 || this.rays[i].angle < 90) {
+                    largestVector.x = 1;
+                }
+
+                if (this.rays[i].angle > 90 && this.rays[i].angle <= 270)
+                    largestVector.y *= -1;
+
+                //same as setMag to get actually distance to move back
+                largestVector.mult(this.rays[i].maxLength / largestVector.mag());
+                //this.speed / 10 to get how much of the bounce back vector to move
+                let pushVector = new Vector(0, 0);
+                pushVector.x = (this.speed / 1) * (largestVector.x - (this.rays[i].posEnd.x - this.rays[i].pos.x))
+                pushVector.y = (this.speed / 1) * (largestVector.y - (this.rays[i].posEnd.y - this.rays[i].pos.y));
+                this.vel.sub(pushVector);
+                
+                totalVector.add(pushVector);
+                //if (this.rays[i].length < 10)
+                 //   this.crashed = true;
+            }
+            if(totalVector.mag() > precision)
+                car.vel.mult(.6);
+            car.slowDown = 1 + totalVector.mag();
+        }
+    }
+
+    draw() {
+        //draw rays
+        Ray.preDraw();
+        for (let i = 0; i < this.rays.length; i++) {
+            this.rays[i].draw();
+        }
+
+        //draw car
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this._angle);
+        rectMode(CENTER);
+        if (this.forwardWay)
+            fill("grey");
+        else
+            fill("red");
+        rect(0, 0, 20, 40);
+        pop();
+    }
+}
---
+++ Tracer Racer (EaselJS)/main.js
@@ -0,0 +1,144 @@
+const overflow = 10000; //10k
+const precision = 0.0001; // 10kths
+
+let objects = [];
+let terrain = [];
+let car;
+
+let mouse = new Vector();
+let mouseDown = false;
+let keys = [];
+
+let score = new createjs.Container();
+
+let pressedDown = false;
+
+function init() {
+    canvas = document.getElementById("game");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight + 1;
+    stage = new createjs.Stage(canvas);
+
+    //player stuff
+    stage.addEventListener("stagemousemove", moveCanvas);
+    stage.addEventListener("stagemousedown", mousePressed);
+    stage.addEventListener("stagemouseup", mouseReleased);
+
+    for (let i = 0; i < terrainPos.length; i++) {
+        terrain.push(new Border(terrainPos[i].x1, terrainPos[i].y1, terrainPos[i].x2, terrainPos[i].y2));
+    }
+
+    lastLine.x = terrainPos[leftIndex].x2;
+    lastLine.y = terrainPos[leftIndex].y2;
+
+    // terrain.push(new Border(terrain));
+
+    ray = new Ray(canvas.width / 2, canvas.height / 2, 270); //x, y, angle
+    //car = new Car(3700, 1000, 12);
+    if (localStorage.getItem("x") && localStorage.getItem("y"))
+        car = new Car(parseFloat(localStorage.getItem("x")), parseFloat(localStorage.getItem("y")), 12);
+    else
+        car = new Car(260, 600, 12);
+
+
+    createjs.Ticker.timingMode = createjs.Ticker.RAF;
+    createjs.Ticker.addEventListener("tick", tick);
+}
+
+function tick(e) {
+    //textSize(20);
+
+    //text(car.loops, 30, 30);
+    //text("time: " + Math.round(car.currentTime * 100) / 100, 30, 55);
+    //text("best time: " + car.bestTime, 30, 80);
+
+    //camera(car.pos.x - width / 2, car.pos.y - height / 2, 0, 0, 0, 0, 1, 0)
+    ////translate(100, 100);
+    //scale(1);
+    ////box border
+    //line(0, 0, 4800, 0);
+    //line(0, 0, 0, 3000);
+    //line(4800, 0, 4800, 3000);
+    //line(0, 3000, 4800, 3000);
+
+    //line(0, 1000, 4800, 1000);
+    //line(0, 2000, 4800, 2000);
+    //line(1600, 0, 1600, 3000);
+    //line(3200, 0, 3200, 3000);
+    
+    //stroke("white");
+    //line(50, 600, 430, 600);
+
+    ////draw
+    //for (let i = 0; i < objects.length; i++) {
+    //    objects[i].draw();
+    //}
+
+    //for (let i = 0; i < terrain.length; i++) {
+    //    terrain[i].draw();
+    //}
+
+    //let compression = 0;
+
+    //if (mouseIsPressed || pressedDown) {
+    //    car.acc.y -= car.speed;
+    //}
+    //if (keyIsDown(83)) {
+    //    car.acc.y += car.speed * 0.5; //reverse
+    //}
+    //if (keyIsDown(65)) {
+    //    car.angle -= 2 * (1 + car.vel.mag() / 40);
+
+    //}
+    //if (keyIsDown(68)) {
+    //    car.angle += 2 * (1 + car.vel.mag() / 40);
+    //}
+
+    //let mouseVec = new Vector(mouseX, mouseY);
+
+    //mouseVec.x -= width / 2;
+    //mouseVec.y -= height / 2;
+    //car._angle = mouseVec.heading() + Math.PI / 2;
+
+    //car.process(terrain);
+    //car.draw();
+
+    //localStorage.setItem("x", car.pos.x);
+    //localStorage.setItem("y", car.pos.y);
+
+    stage.update(event);
+}
+
+
+function moveCanvas(e) {
+    mouse.x = e.stageX;
+    mouse.y = e.stageY;
+}
+
+function mousePressed(e) {
+    mouseDown = true;
+}
+
+function mouseReleased(e) {
+    mouseDown = false;
+}
+
+onkeydown = onkeyup = function (e) {
+    e = e || event; // to deal with IE
+    keys[e.keyCode] = e.type == 'keydown';
+    if (e.keyCode = 32 && keys[32]) {
+        car.vel.mult(0);
+        car.pos.x = 260;
+        car.pos.y = 600;
+        car.angle = 0;
+    }
+}
+window.addEventListener('touchstart', function() {
+    mouseDown = true;
+});
+
+window.addEventListener('touchend', function() {
+    mouseDown = false;
+});
+
+//window.addEventListener('touchmove', function(e){e.preventDefault()});
