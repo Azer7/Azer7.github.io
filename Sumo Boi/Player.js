d89a935da29defd0b95f14ad19e5f28e1bef34a2
Azer7 <Azer7@users.noreply.github.com>
2018-03-22 19:40:44 -0600

    ye

--- Sumo Boi/Player.js
+++ Sumo Boi/Player.js
@@ -1,19 +1,19 @@
-class Sumo {
+class Player {
     constructor(x, y, size) {
-        this.speed = .35;
+        this.speed = .35; 
         this.pos = new Vector(x, y);
         this.vel = new Vector(0, 0);
         this.acc = new Vector(0, 0);
 
-        this.rayNum = 16;
-
-        for (let i = 0; i < rayAmount; i++)
-            this.rays.push(new Ray(0, 0, i * (360 / rayAmount)));
+        this.rayAmount = 16;
+        this.rays = [];
+        for (let i = 0; i < this.rayAmount; i++)
+            this.rays.push(new Ray(0, 0, i * (360 / this.rayAmount)));
 
         this.g = new createjs.Shape();
         this.g.graphics.setStrokeStyle(1).beginStroke("black");
         this.fill = this.g.graphics.beginFill("red");
-        this.g.graphics.drawCircle(15,15,30);
+        this.g.graphics.drawCircle(0,0,30);
         
         stage.addChild(this.g)
     }
--- Sumo Boi/main.js
+++ Sumo Boi/main.js
@@ -1,8 +1,21 @@
+let mouse = {
+    x: 0,
+    y: 0,
+    down: false
+};
+
+let player;
+
 function init() {
     canvas = document.getElementById("game");
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.height = window.innerHeight + 1;
     stage = new createjs.Stage(canvas);
+
+    stage.on("stagemousemove", moveCanvas);
+    
+    
+    player = new Player(0, 0, 50);
 
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.on("tick", tick);
@@ -10,4 +23,10 @@ function init() {
 
 function tick(e) {
 
+    stage.update();
+}
+
+function moveCanvas(e) {
+    mouse.x = e.stageX;
+    mouse.y = e.stageY;
 }
